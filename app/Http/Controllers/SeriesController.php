6bc32d02306ea0f4df9a4440cbb26e721b6a96fa
Rodrigo Estabel Boquer <rboquer@emater.tche.br>
2025-10-29 08:53:36 -0300

    first commit

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SeriesController extends Controller
+{
+    public function index()
+    {
+        $series = [
+            'Punisher',
+            'Lost',
+            'Grey\'s Anatomy'
+        ];
+
+        // return view('listar-series', [
+        //     'series' => $series
+        // ]);
+
+        // $html = '<ul>';
+        // foreach ($series as $serie) {
+        //     $html .= "<li>$serie</li>";
+        // }
+        // $html .= '</ul>';
+
+        // return $html;
+
+        // return view('listar-series', compact('series'));
+
+        return view('series.index')->with('series', $series);
+    }
+}
