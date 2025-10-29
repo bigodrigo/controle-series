1c315adcecb59791f2b62b8374f1f34daf505075
Rodrigo Estabel Boquer <rboquer@emater.tche.br>
2025-10-29 15:10:21 -0300

    non-ideal db integration

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -2,18 +2,22 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SeriesController extends Controller
 {
     public function index()
     {
-        $series = [
-            'Punisher',
-            'Lost',
-            'Grey\'s Anatomy'
-        ];
+        // $series = [
+        //     'Punisher',
+        //     'Lost',
+        //     'Grey\'s Anatomy'
+        // ];
+        $series = DB::select('SELECT nome FROM series;');
+
 
         // return view('listar-series', [
         //     'series' => $series
@@ -34,5 +38,16 @@ class SeriesController extends Controller
 
     public function create() {
         return view('series.create');
+    }
+
+    public function store(Request $request) {
+        $nomeSerie = $request->input('nome');
+
+        if (DB::insert('INSERT INTO series (nome) VALUES (?)', [$nomeSerie])) {
+                return "OK";
+        } else {
+            return "Deu erro";
+        }
+
     }
 }
