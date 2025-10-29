d8f5dd751869a3887a1b2c9e044d543093dac6fe
Rodrigo Estabel Boquer <rboquer@emater.tche.br>
2025-10-29 11:25:07 -0300

    vite migration to mix

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class SeriesController extends Controller
@@ -29,5 +30,9 @@ class SeriesController extends Controller
         // return view('listar-series', compact('series'));
 
         return view('series.index')->with('series', $series);
+    }
+
+    public function create() {
+        return view('series.create');
     }
 }
