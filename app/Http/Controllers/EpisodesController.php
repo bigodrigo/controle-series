5867cefa0a7ff59b8aacf42bb46c07fbf416d5ce
bigodrigo <roesbo1@hotmail.com>
2025-11-03 14:26:46 -0300

    success message in layout

--- app/Http/Controllers/EpisodesController.php
+++ app/Http/Controllers/EpisodesController.php
@@ -14,7 +14,10 @@ class EpisodesController
 
     public function index(Season $season)
     {
-        return view('episodes.index', ['episodes' => $season->episodes]);
+        return view('episodes.index', [
+            'episodes' => $season->episodes,
+            'mensagemSucesso' => session('mensagem.sucesso')
+        ]);
     }
 
     public function update(Request $request, Season $season) {
@@ -32,7 +35,7 @@ class EpisodesController
             }
         });
 
-        return to_route('episodes.index', ['season' => $season->id])
+        return to_route('episodes.index', $season->id)
             ->with('mensagem.sucesso', 'Episódios atualizados com sucesso!');
     }
 }
