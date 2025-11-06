502a6f78eb521de69d9ad5a2c4b2256aca81603c
bigodrigo <roesbo1@hotmail.com>
2025-11-06 11:00:53 -0300

    basic CRUD

--- app/Http/Controllers/api/SeriesController.php
+++ app/Http/Controllers/api/SeriesController.php
@@ -5,14 +5,41 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\SeriesFormRequest;
 use App\Models\Series;
+use App\Repositories\SeriesRepository;
 
 class SeriesController extends Controller {
+    public function __construct(private SeriesRepository $seriesRepository)
+    {
+
+    }
+
     public function index() {
         return Series::all();
     }
 
     public function store(SeriesFormRequest $request) {
         return response()
-            ->json(Series::create($request->all()), 201);
+            ->json($this->seriesRepository->addSeries($request), 201);
+    }
+
+    // public function show(int $series) {      // Séries + temp + ep
+    //     $series = Series::whereId($series)->with('seasons.episodes')->first();
+    //     return $series;
+    // }
+
+    public function show(Series $series) {
+	    return $series;
+    }
+
+    public function update(Series $series, SeriesFormRequest $request) {
+        $series->fill($request->all());
+        $series->save();
+
+        return $series;
+    }
+
+    public function destroy(int $series) {
+        Series::destroy($series);
+        return response()->noContent();
     }
 }
