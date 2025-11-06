486f9ea71d9341b9babbafbdc753b43cf94d3b7f
bigodrigo <roesbo1@hotmail.com>
2025-11-06 10:13:34 -0300

    api initial config

---
+++ app/Http/Controllers/api/SeriesController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\SeriesFormRequest;
+use App\Models\Series;
+
+class SeriesController extends Controller {
+    public function index() {
+        return Series::all();
+    }
+
+    public function store(SeriesFormRequest $request) {
+        return response()
+            ->json(Series::create($request->all()), 201);
+    }
+}
